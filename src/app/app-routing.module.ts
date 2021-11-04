2299d81c45ca95d9b4d7f61102b413288cb85e59
zainuleb <p166024@nu.edu.pk>
2021-11-04 23:34:40 +0500

    Initiating Angular Project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListingsPageComponent } from './listings-page/listings-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'listings', component: ListingsPageComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
